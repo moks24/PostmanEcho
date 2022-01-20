578051d08225d706af79adcd3982c5f7840db632
Moks <gutamax2@mail.ru>
2022-01-20 15:53:17 +0500

    fix error

--- src/test/java/ru/nethology/PostmanTest.java
+++ src/test/java/ru/nethology/PostmanTest.java
@@ -19,7 +19,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"));
+                .body("data", equalTo("some data"));
 
     }
 }
